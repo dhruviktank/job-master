38214db1415e4a771e3dcc24723c5c88991ee453
Dhruvik Tank <dhruvik29@outlook.com>
2025-05-03 12:33:48 +0530

    JS-0004: updated redist host

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    redis_host: str = "localhost"
+    redis_host: str = "job-master-1.onrender.com"
     redis_port: int = 6379
     app_name: str = "Scraper API"
 
