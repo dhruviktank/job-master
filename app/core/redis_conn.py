480ec802f9159bd613c80891ebabfce83540a187
Dhruvik Tank <dhruvik29@outlook.com>
2025-05-03 13:58:59 +0530

    JS-0007: updated redis connection

--- app/core/redis_conn.py
+++ app/core/redis_conn.py
@@ -2,7 +2,13 @@ from redis import Redis
 from rq import Queue
 from app.core.config import settings
 
-redis_conn = Redis(host=settings.redis_host, port=settings.redis_port)
+redis_conn = Redis(
+    host='redis-17369.c16.us-east-1-3.ec2.redns.redis-cloud.com',
+    port=17369,
+    decode_responses=True,
+    username="default",
+    password="K7QZEul2GgrsxBr2koiZC4m5sR3ULGfG"
+)
 
 request_queue = Queue('request_queue', connection=redis_conn)
 linkedin_mainQ = Queue('linkedin_main_scraper', connection=redis_conn)
